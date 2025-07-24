232f78ca4826f67880fe81daf782c8cf6837dfd2
Lucas Costa <94561395+luxas12300@users.noreply.github.com>
2025-07-24 19:14:42 -0300

    OrderStatus enum

--- course/src/main/java/com/lucas_costa/course/entities/Order.java
+++ course/src/main/java/com/lucas_costa/course/entities/Order.java
@@ -2,6 +2,7 @@ package com.lucas_costa.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.lucas_costa.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -17,15 +18,18 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH-mm-ss'Z'", timezone = "GMT")
     private Instant instant;
+
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order(){}
 
-    public Order(Long id, Instant instant, User client) {
+    public Order(Long id, Instant instant,OrderStatus orderStatus, User client) {
         this.id = id;
         this.instant = instant;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -43,6 +47,16 @@ public class Order implements Serializable {
 
     public void setInstant(Instant instant) {
         this.instant = instant;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
