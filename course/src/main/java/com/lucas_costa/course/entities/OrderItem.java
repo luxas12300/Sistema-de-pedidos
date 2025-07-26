585cc3cb63cd0a26acd47bc93febc3873536a58e
Lucas Costa <94561395+luxas12300@users.noreply.github.com>
2025-07-26 15:17:31 -0300

    Payment, one-to-one association

--- course/src/main/java/com/lucas_costa/course/entities/OrderItem.java
+++ course/src/main/java/com/lucas_costa/course/entities/OrderItem.java
@@ -38,6 +38,7 @@ public class OrderItem implements Serializable {
     public void setProduct(Product product){
         id.setProduct(product);
     }
+
     public Product getProduct(){
         return id.getProduct();
     }
